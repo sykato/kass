e8a34c5e4b6706604af047477951c037d44a4758
Tereschenko Aleksandr <tere@WIN-JUB8CCCU1GA.DOMAIN.local>
2022-02-26 11:33:33 +0300

    class

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿class person
+{
+    public string name
+    { get; set; }
+    public string surname
+    { get; set; }
+    public DateTime daterod
+    { get; set; }
+    public person (string name, string surname, DateTime daterod) 
+    {
+        this.name = name;
+        this.surname = surname;
+        this.daterod = daterod;
+    }
+enum Education
+{
+    Specialist,
+    Вachelor,
+    SecondEducation
+}
+class exam
+{
+        public string predmet;
+        public int ocenka;
+        public DateTime dateexam;
+    public exam(string predmet, int ocenka, DateTime dateexam)
+    {
+        this.predmet = predmet;
+        this.ocenka = ocenka;
+        this.dateexam = dateexam;
+    }
+    private exam()
+    {
+        predmet = "defolt_predmet";
+        ocenka = 5;
+        dateexam = new DateTime(26, 02, 22);
+    }
+        public string ToFullstring()
+        {
+            return string.Format(predmet, ocenka, dateexam);
+        }
+}
+
+class student
+{
+        private person person;
+        private Education education;
+        private int nomergrypp;
+}
